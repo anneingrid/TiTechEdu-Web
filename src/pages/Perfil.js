10039c81c9c036652a269f75a578971cd847d4c8
AureaFerreira <aureaferreiraf11@gmail.com>
2023-11-06 16:48:06 -0300

    rotas

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,61 +1,65 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Image, Button, Form } from 'react-bootstrap';
+import { Container, Row, Col, Image, Form } from 'react-bootstrap';
 import Footerr from './Footerr';
 import NavBarr from './NavBarr';
-
 
 export default function Perfil() {
     return (
         <>
             <NavBarr></NavBarr>
             <h3>Home/Perfil</h3>
-            <Container>
-                <Row>
-                    <Col xs={6} md={4}>
+            <Container className="perfil-container">
+                <Row className="justify-content-center align-items-center">
+                    <Col xs={6} md={4} className="text-center">
                         <Image
                             src="https://via.placeholder.com/200"
                             roundedCircle
-                            className="perfil-image" 
+                            className="perfil-image"
                         />
                     </Col>
                 </Row>
             </Container>
 
-            <Form>
-                <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formNome">
-                        <Form.Label className="form-label">Nome</Form.Label>
-                        <Form.Control type="text" placeholder="Digite seu nome" className="form-input" />
-                    </Form.Group>
+            <Container className="perfil-container">
+                <Form>
+                    <Row className="mb-3">
+                        <Col xs={12} md={6}>
+                            <Form.Group controlId="formNome">
+                                <Form.Label className="form-label">Nome</Form.Label>
+                                <Form.Control type="text" placeholder="Digite seu nome" className="form-input" />
+                            </Form.Group>
+                        </Col>
 
-                    <Form.Group as={Col} controlId="formSobrenome">
-                        <Form.Label className="form-label">Sobrenome</Form.Label>
-                        <Form.Control type="text" placeholder="Digite seu sobrenome" className="form-input" />
-                    </Form.Group>
-                </Row>
+                        <Col xs={12} md={6}>
+                            <Form.Group controlId="formSobrenome">
+                                <Form.Label className="form-label">Sobrenome</Form.Label>
+                                <Form.Control type="text" placeholder="Digite seu sobrenome" className="form-input" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
 
-                <Form.Group className="mb-3" controlId="formIdioma">
-                    <Form.Label className="form-label">Idioma</Form.Label>
-                    <Form.Select className="form-input">
-                        <option value="portuguese">Português</option>
-                        <option value="english">English</option>
-                        <option value="spanish">Español</option>
-                    </Form.Select>
-                </Form.Group>
+                    <Row className="mb-3">
+                        <Col xs={12} md={6}>
+                            <Form.Group controlId="formIdioma">
+                                <Form.Label className="form-label">Idioma</Form.Label>
+                                <Form.Select className="form-input">
+                                    <option value="portuguese">Português</option>
+                                    <option value="english">English</option>
+                                    <option value="spanish">Español</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
 
-                <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridCity">
-                        <Form.Label className="form-label">Cidade</Form.Label>
-                        <Form.Control type="text" placeholder="Digite sua cidade" className="form-input" />
-                    </Form.Group>
-
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label className="form-label">Estado</Form.Label>
-                        <Form.Control type="text" placeholder="Digite seu estado" className="form-input" />
-                    </Form.Group>
-                </Row>
-            </Form>
+                        <Col xs={12} md={6}>
+                            <Form.Group controlId="formGridCity">
+                                <Form.Label className="form-label">Cidade</Form.Label>
+                                <Form.Control type="text" placeholder="Digite sua cidade" className="form-input" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
             <Footerr></Footerr>
         </>
     );
