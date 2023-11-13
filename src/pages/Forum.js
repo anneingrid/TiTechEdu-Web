4cee64c1989a112663f3f0ce0673909f31fe9168
aaad <aaa@gmail.com>
2023-11-13 18:27:24 -0300

    feat: inserindo página fórum e integrando com banco

--- src/pages/Forum.js
+++ src/pages/Forum.js
@@ -1,10 +1,33 @@
+import { Button, Container } from "react-bootstrap";
 import NavBarr from "./NavBarr";
+import CardForum from "../components/CardForum";
+import ModalPergunta from "../components/ModalPergunta";
+import { useState } from "react";
+
 
 export default function Forum() {
+    const [showModal, setShowModal] = useState(false);
+
+    const abrirModal = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
     return (
         <>
-            <NavBarr></NavBarr>
-            <h1>Nada aqui.</h1>
+            <NavBarr />
+            <Container style={{ marginTop: '2rem' }}>
+                <h1 className="text-light">
+                    Fórum
+                </h1>
+                <CardForum />
+
+            </Container>
+            <ModalPergunta />
+
         </>
     );
 };
