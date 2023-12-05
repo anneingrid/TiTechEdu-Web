d8871e72ce8098afeabcb890013a8fc770a5bdfa
Victor Dias <lzero1541@gmail.com>
2023-12-05 02:41:55 -0300

    feat: conectando o backend com o frontend

--- src/pages/Vagas.js
+++ src/pages/Vagas.js
@@ -3,54 +3,24 @@ import Footerr from "./Footerr";
 import "./Vagas.css";
 import Lottie from "lottie-react";
 import animationData from "./animacaoVaga.json";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Vagas() {
-  let listaVagas = [
-    {
-      linkVaga:
-        "https://br.linkedin.com/jobs/view/desenvolvedor-front-end-wordpress-home-office-at-netvagas-3769151403?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
-      imagemVaga:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_FT1TjjfButeY0KVylqZuN4gHoSrFsfPP10cG5Zk&s",
-      nomeVaga: "Desenvolvedor front end wordpress home office",
-      empresalVaga: "Netvagas",
-      localVaga: "Qualquer lugar",
-      descricaoVaga:
-        "Formação em Sistemas de Informação ou áreas correlacionadas. Domínio de HTML, CSS e JavaScrip...",
-    },
-    {
-      linkVaga:
-        "https://glassdoor.com.br/job-listing/analista-de-ti-sistemas-mainframe-volkswagen-do-brasil-JV_IC2481826_KO0,33_KE34,54.htm?jl=1008985241412&utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
-      imagemVaga:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEfirdFS0HQDyBKLzWv_1uCGHplDMhzkd3Dq9FVgg&s",
-      nomeVaga: "Analista de TI - Sistemas Mainframe",
-      empresalVaga: "Volkswagen do Brasil",
-      localVaga: "Qualquer lugar",
-      descricaoVaga:
-        "O candidato será responsável técnico pelos Sistemas Mainframe da Volkswagen do Brasil por meio de análise... ",
-    },
-    {
-      linkVaga:
-        "https://www.turing.com/pt/jobs/desenvolvedor-c-remoto?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organicgit",
-      imagemVaga:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6rGChs-3HBqRPulfLo4-_2uO_gWol6U1OAmgb01Y&s",
-      nomeVaga: "Vagas C | Ganhe em Dólar | Turing Jobs",
-      empresalVaga: "Turing.com",
-      localVaga: "Qualquer lugar",
-      descricaoVaga:
-        "Nós, da Turing, estamos procurando preencher vagas C, onde os desenvolvedores experientes irão projetar...",
-    },
-    {
-      linkVaga:
-        "https://glassdoor.com.br/job-listing/it-analyst-i-santander-JV_IC2479061_KO0,12_KE13,22.htm?jl=1008986354942&utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
-      imagemVaga:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLGRL6TADuPskUdE8bPj8SykM-PlbiVEaiQvuhl2s&s",
-      nomeVaga: "IT Analyst I",
-      empresalVaga: "Santander",
-      localVaga: "Qualquer lugar",
-      descricaoVaga:
-        "Fazemos parte do maior banco Internacional do Brasil, um dos maiores bancos do mundo...",
-    },
-  ];
+  const [listaVagas, setListaVagas] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/jobs")
+      .then((response) => {
+        setListaVagas(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao fazer a requisição:", error);
+      });
+  }, []);
+
+  if (listaVagas.length === 0) return <div>Carregando...</div>;
 
   return (
     <>
@@ -68,21 +38,21 @@ export default function Vagas() {
             {listaVagas.map((vaga, index) => (
               <li style={{ "--i": index + 1 }} key={index}>
                 <img
-                  src={vaga.imagemVaga}
+                  src={vaga.imageURL}
                   alt="Imagem da empresa da Vaga"
                   className="vagas-imagem"
                 />
                 <div className="content">
-                  <h4 className="vaga-titulo">{vaga.nomeVaga}</h4>
+                  <h4 className="vaga-titulo">{vaga.title}</h4>
                   <p className="vaga-texto">
-                    {vaga.empresalVaga} <span>{vaga.localVaga}</span>
+                    {vaga.nameCompany} <span>{vaga.location}</span>
                   </p>
-                  <p className="vaga-descricao">{vaga.descricaoVaga}</p>
+                  <p className="vaga-descricao">{vaga.description}</p>
                 </div>
                 <div className="vagas-container-botao">
                   <a
                     className="vagas-button"
-                    href={vaga.linkVaga}
+                    href={vaga.link}
                     target="_blank"
                     rel="noreferrer"
                   >
